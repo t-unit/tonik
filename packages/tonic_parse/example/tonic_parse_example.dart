271e15d3826a63c973707fac4475b2f49ce25df4
Tobias Ottenweller <tobi@ottenweller.net>
2024-12-07 17:20:57 +0100

    chore: setup linting

--- packages/tonic_parse/example/tonic_parse_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:tonic_parse/tonic_parse.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- packages/tonic_parse/lib/tonic_parse.dart
+++ packages/tonic_parse/lib/tonic_parse.dart
@@ -1,8 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Loading, parsing and processing of Openapi files.
 library;
-
-export 'src/tonic_parse_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
