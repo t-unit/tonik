42c5c0e421893bd2a303ee27e042bfa78d0f2a06
Tobias Ottenweller <tobi@ottenweller.net>
2025-02-13 19:34:00 +0100

    refactor: introduce server importer

--- packages/tonic_parse/lib/src/importer.dart
+++ packages/tonic_parse/lib/src/importer.dart
@@ -1,6 +1,7 @@
 import 'package:tonic_core/tonic_core.dart';
 import 'package:tonic_parse/src/model/open_api_object.dart';
 import 'package:tonic_parse/src/model_importer.dart';
+import 'package:tonic_parse/src/server_importer.dart';
 
 class Importer {
   ApiDocument import(Map<String, dynamic> fileContent) {
@@ -11,19 +12,7 @@ class Importer {
       version: openApiObject.info.version,
       description: openApiObject.info.description,
       models: ModelImporter(openApiObject).import(),
-      servers: _importServers(openApiObject),
+      servers: ServerImporter(openApiObject: openApiObject).import(),
     );
-  }
-
-  Set<Server> _importServers(OpenApiObject openApiObject) {
-    final servers = openApiObject.servers
-        ?.map(
-          (server) => Server(
-            url: server.url,
-            description: server.description,
-          ),
-        )
-        .toSet();
-    return servers ?? {};
   }
 }
