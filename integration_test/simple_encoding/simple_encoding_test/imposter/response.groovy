e64e9e08e89e7a509651c768e4514ccb2871c130
Tobias Ottenweller <tobi@ottenweller.net>
2025-08-10 16:02:01 +0200

    chore: simple encoding integration tests

---
+++ integration_test/simple_encoding/simple_encoding_test/imposter/response.groovy
@@ -0,0 +1,7 @@
+// Get the response status from the request header
+def responseStatus = context.request.headers['X-Response-Status'] ?: '200'
+
+// Set the response status code and use the OpenAPI specification
+respond()
+    .withStatusCode(Integer.parseInt(responseStatus))
+    .usingDefaultBehaviour()
