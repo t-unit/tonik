6f61d629850b716da3ccb2b9d6c9fdf46b634de4
Tobias Ottenweller <tobi@ottenweller.net>
2026-07-09 08:15:15 +0200

    Fix structured syntax suffix fixture response

--- integration_test/structured_syntax_suffix/structured_syntax_suffix_test/imposter/response.groovy
+++ integration_test/structured_syntax_suffix/structured_syntax_suffix_test/imposter/response.groovy
@@ -1,26 +1,33 @@
-def response = respond().withStatusCode(200)
-
 if (context.request.path == '/api/v1/widget' && context.request.method == 'GET') {
-    response.withHeader('Content-Type', 'application/vnd.api+json')
-            .withContent('{"id":42,"name":"sprocket"}')
+    respond()
+        .withStatusCode(200)
+        .withHeader('Content-Type', 'application/vnd.api+json')
+        .withContent('{"id":42,"name":"sprocket"}')
 
 } else if (context.request.path == '/api/v1/problem' && context.request.method == 'GET') {
-    response.withHeader('Content-Type', 'application/problem+json')
-            .withContent('{"id":7,"name":"teapot"}')
+    respond()
+        .withStatusCode(200)
+        .withHeader('Content-Type', 'application/problem+json')
+        .withContent('{"id":7,"name":"teapot"}')
 
 } else if (context.request.path == '/api/v1/wildcard/application' && context.request.method == 'GET') {
-    response.withHeader('Content-Type', 'application/json')
-            .withContent('{"id":99,"name":"application-wildcard"}')
+    respond()
+        .withStatusCode(200)
+        .withHeader('Content-Type', 'application/json')
+        .withContent('{"id":99,"name":"application-wildcard"}')
 
 } else if (context.request.path == '/api/v1/wildcard/catch-all' && context.request.method == 'GET') {
-    response.withHeader('Content-Type', 'text/plain')
-            .withContent('catch-all wildcard response')
+    respond()
+        .withStatusCode(200)
+        .withHeader('Content-Type', 'text/plain')
+        .withContent('catch-all wildcard response')
 
 } else if (context.request.path == '/api/v1/wildcard/range-status' && context.request.method == 'GET') {
-    response.withStatusCode(206)
-            .withHeader('Content-Type', 'application/json')
-            .withContent('{"id":206,"name":"range-wildcard"}')
+    respond()
+        .withStatusCode(206)
+        .withHeader('Content-Type', 'application/json')
+        .withContent('{"id":206,"name":"range-wildcard"}')
 
 } else {
-    response.usingDefaultBehaviour()
+    respond().usingDefaultBehaviour()
 }
