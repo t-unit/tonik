feb23676b53eefc5d817c85d8a741ec4fc9e6e2f
Tobias Ottenweller <tobi@ottenweller.net>
2026-07-30 22:00:26 +0200

    feat: generate HTTP request bodies and headers

---
+++ packages/tonik_generate/lib/src/transport/http/http_headers_generator.dart
@@ -0,0 +1,75 @@
+import 'package:code_builder/code_builder.dart';
+import 'package:tonik_core/tonik_core.dart';
+import 'package:tonik_generate/src/naming/name_manager.dart';
+import 'package:tonik_generate/src/transport/request_headers_generator.dart';
+import 'package:tonik_generate/src/util/spec_literal_string.dart';
+
+/// Generates the exact string header map applied to a `package:http` request.
+class HttpHeadersGenerator {
+  const HttpHeadersGenerator({
+    required this.nameManager,
+    required this.package,
+    this.useImmutableCollections = false,
+  });
+
+  final NameManager nameManager;
+  final String package;
+  final bool useImmutableCollections;
+
+  Method generateHeadersMethod(
+    Operation operation,
+    List<({String normalizedName, RequestHeaderObject parameter})> headers,
+    List<({String normalizedName, CookieParameterObject parameter})>
+    cookieParameters,
+  ) {
+    final stringType = refer('String', 'dart:core');
+    final generated = RequestHeadersGenerator(
+      nameManager: nameManager,
+      package: package,
+      headerValueType: stringType,
+      useImmutableCollections: useImmutableCollections,
+    ).generate(operation, headers, cookieParameters);
+    final statements = <Code>[...generated.statements];
+    final requestBody = operation.requestBody;
+    final content = requestBody?.resolvedContent.toList() ?? const [];
+
+    if (generated.contentType != null &&
+        content.isNotEmpty &&
+        content.singleOrNull?.contentType != ContentType.multipart) {
+      final assignContentType = refer(r'_$headers')
+          .index(specLiteralString('Content-Type'))
+          .assign(generated.contentType!)
+          .statement;
+      if (requestBody!.isRequired && content.length == 1) {
+        statements.add(assignContentType);
+      } else {
+        statements.add(
+          Block.of([
+            const Code(r'if (_$contentType != null) {'),
+            refer(r'_$headers')
+                .index(specLiteralString('Content-Type'))
+                .assign(refer(r'_$contentType'))
+                .statement,
+            const Code('}'),
+          ]),
+        );
+      }
+    }
+
+    statements.add(refer(r'_$headers').returned.statement);
+
+    return Method(
+      (b) => b
+        ..name = '_options'
+        ..returns = TypeReference(
+          (t) => t
+            ..symbol = 'Map'
+            ..url = 'dart:core'
+            ..types.addAll([stringType, stringType]),
+        )
+        ..optionalParameters.addAll(generated.parameters)
+        ..lambda = false
+        ..body = Block.of(statements),
+    );
+  }
+}
